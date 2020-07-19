3f6521c38efc65fc94fc51c6d29d4a1262c187a4
diejee <jandouweesminia@gmial.com>
2020-07-19 17:26:55 +0200

    main menu
    Main Menu = start aan player

---
+++ Unity360VideoPlayer/Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void showVideos() {
+        SceneManager.LoadScene("videos");
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+
+    public void settings(){
+        SceneManager.LoadScene("settings");
+    }
+}
